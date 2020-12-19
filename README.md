9e7dd68b235874d1064bf9f506a1957f149ac456
Solla <willieyuwei4@gmail.com>
2020-12-20 01:35:33 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Acknowledgement
+This is a variant from BOLL7708's project [OpenVRInputTest](https://github.com/BOLL7708/OpenVRInputTest) .
+
+
+# Controller Disabler
+Lots of people feel annoyed at the controller vibrating.
+
+I have found several posts on the internet about how to disable the vibration of controllers.
+
+Example:
+
+[Is there a way to disable controller vibration?](https://www.reddit.com/r/WindowsMR/comments/8obiml/is_there_a_way_to_disable_controller_vibration/)
+
+[Its possible to disable controller vibration at all?](https://www.reddit.com/r/Vive/comments/94noui/its_possible_to_disable_controller_vibration_at/e3n8e7d/)
+
+[How to disable all haptics?](https://www.reddit.com/r/WindowsMR/comments/ds7qna/how_to_disable_all_haptics/)
+
+[How to disable rumble/vibrate feature in controllers?](https://www.reddit.com/r/WindowsMR/comments/9gtafh/how_to_disable_rumblevibrate_feature_in/)
+
+[How do I turn off Oculus Touch controller vibration?](https://forums.oculusvr.com/community/discussion/69874/how-do-i-turn-off-oculus-touch-controller-vibration)
+
+## How It Works
+The program is based on OpenVR API, and it keeps sending vibration signals with ``amplitude: 0``, which means stop the vibration immediately.
+
+Even if games send a rumble request to controllers, this program will let the vibration stop without delay.
+
+Sometimes the controllers are clattering, but the vibration is imperceptible.
+
+# How To Use
+
+Just open **VibrationDisable.exe**.
---
+++ VibrationDisable/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Valve.VR;
+//Sources: https://github.com/BOLL7708/OpenVRInputTest
+namespace OpenVRInputTest
+{
+    class Program
+    {
+        static ulong mActionSetHandle;
+        static VRActiveActionSet_t[] mActionSetArray;
+
+        // # items are referencing this list of actions: https://github.com/ValveSoftware/openvr/wiki/SteamVR-Input#getting-started
+        static void Main(string[] args)
+        {
+            // Initializing connection to OpenVR
+            var error = EVRInitError.None;
+            OpenVR.Init(ref error, EVRApplicationType.VRApplication_Background); // Had this as overlay before to get it working, but changing it back is now fine?
+            var workerThread = new Thread(Worker);
+            if (error != EVRInitError.None)
+                Utils.PrintError($"OpenVR initialization errored: {Enum.GetName(typeof(EVRInitError), error)}");
+            else
+            {
+                Utils.PrintInfo("OpenVR initialized successfully.");
+
+                // Load app manifest, I think this is needed for the application to show up in the input bindings at all
+                Utils.PrintVerbose("Loading app.vrmanifest");
+                var appError = OpenVR.Applications.AddApplicationManifest(Path.GetFullPath("./app.vrmanifest"), false);
+                if (appError != EVRApplicationError.None)
+                    Utils.PrintError($"Failed to load Application Manifest: {Enum.GetName(typeof(EVRApplicationError), appError)}");
+                else 
+                    Utils.PrintInfo("Application manifest loaded successfully.");
+
+                // #3 Load action manifest
+                Utils.PrintVerbose("Loading actions.json");
+                var ioErr = OpenVR.Input.SetActionManifestPath(Path.GetFullPath("./actions.json"));
+                if (ioErr != EVRInputError.None) 
+                    Utils.PrintError($"Failed to load Action Manifest: {Enum.GetName(typeof(EVRInputError), ioErr)}");
+                else 
+                    Utils.PrintInfo("Action Manifest loaded successfully.");
+
+                // #4 Get action handles
+                Utils.PrintVerbose("Getting action handles");
+                
+                
+                // #5 Get action set handle
+                Utils.PrintVerbose("Getting action set handle");
+                var errorAS = OpenVR.Input.GetActionSetHandle("/actions/default", ref mActionSetHandle);
+                if (errorAS != EVRInputError.None) 
+                    Utils.PrintError($"GetActionSetHandle Error: {Enum.GetName(typeof(EVRInputError), errorAS)}");
+                Utils.PrintDebug($"Action Set Handle default: {mActionSetHandle}");
+
+                // Starting worker
+                Utils.PrintDebug("Starting worker thread.");
+                if (!workerThread.IsAlive) 
+                    workerThread.Start();
+                else 
+                    Utils.PrintError("Could not start worker thread.");
+            }
+            Console.ReadLine();
+            workerThread.Abort();
+            OpenVR.Shutdown();
+        }
+
+        private static void Worker()
+        {
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            Thread.CurrentThread.IsBackground = true;
+            ulong LeftVibration = 0, RightVibration = 0;
+            OpenVR.Input.GetActionHandle("/actions/default/out/haptic_left", ref LeftVibration); 
+            OpenVR.Input.GetActionHandle("/actions/default/out/haptic_right", ref RightVibration);
+            // #6 Update action set
+            if (mActionSetArray == null)
+            {
+                var actionSet = new VRActiveActionSet_t
+                {
+                    ulActionSet = mActionSetHandle,
+                    ulRestrictedToDevice = OpenVR.k_ulInvalidActionSetHandle,
+                    nPriority = 0
+                };
+                mActionSetArray = new VRActiveActionSet_t[] { actionSet };
+            }
+            while (true)
+            {
+                var errorUAS = OpenVR.Input.UpdateActionState(mActionSetArray, (uint)Marshal.SizeOf(typeof(VRActiveActionSet_t)));
+                if (errorUAS != EVRInputError.None) 
+                    Utils.PrintError($"UpdateActionState Error: {Enum.GetName(typeof(EVRInputError), errorUAS)}");
+
+                var errorLeftVibration = OpenVR.Input.TriggerHapticVibrationAction(LeftVibration, 0, 1000, 0, 0, OpenVR.k_ulInvalidInputValueHandle);
+                if (errorLeftVibration != EVRInputError.None)
+                    Utils.PrintError($"Left Vibration Error: {Enum.GetName(typeof(EVRInputError), errorLeftVibration)}");
+                
+                var errorRightVibration = OpenVR.Input.TriggerHapticVibrationAction(RightVibration, 0, 1000, 0, 0, OpenVR.k_ulInvalidInputValueHandle);
+                if (errorRightVibration != EVRInputError.None)
+                    Utils.PrintError($"Right Vibration Error: {Enum.GetName(typeof(EVRInputError), errorRightVibration)}");
+            }
+        }
+    }
+}
+
